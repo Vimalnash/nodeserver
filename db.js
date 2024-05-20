bc8b7530fd28fc23c4fd87ed00cd4946c7347773
Avinash <vimalnash@gmail.com>
2024-05-20 20:19:11 +0530

    nodedeployed

--- db.js
+++ db.js
@@ -1,30 +1,9 @@
 import mongoose from "mongoose";
 
-
-// Method 1 : import dotenv and configure here
-    // import dotenv from "dotenv"
-
-    // dotenv.config();
-
-    // const MONGO_URL_LOCAL = process.env.MONGO_URL_LOCAL;
-    // const MONGO_URL_AtlasCloud = process.env.MONGO_URL_AtlasCloud;
-
-
-// Method 2 : passing as argument in function from index.js
-
-// export function dbConnection() {
-export function dbConnection(MONGO_URL_LOCAL, MONGO_URL_AtlasCloud) {
-
-    // const params = {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    // };
+export function dbConnection(MONGO_URL_AtlasCloud) {
 
     try {
-        // mongoose.connect(MONGO_URL_LOCAL, params);
-        
         mongoose.connect(MONGO_URL_AtlasCloud);
-        // mongoose.connect(MONGO_URL_LOCAL);
         console.log("Database Connected Successfully");
     } catch (error) {
         console.log("MongoDb Connection Error");
